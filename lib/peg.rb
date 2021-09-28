a8444ab97a306e31ad8757e071f71b25f4bdd303
David Albert <davidbalbert@gmail.com>
2021-09-28 19:52:45 -0400

    Fix tabs

--- lib/peg.rb
+++ lib/peg.rb
@@ -1,20 +1,20 @@
 module Peg
   module ModuleAttribute
-  refine Module do
-    def module_attribute(name, default: nil)
-      define_singleton_method name do
-        instance_variable_get "@#{name}"
-      end
+    refine Module do
+      def module_attribute(name, default: nil)
+        define_singleton_method name do
+          instance_variable_get "@#{name}"
+        end
 
-      define_singleton_method "#{name}=" do |new_value|
-        instance_variable_set "@#{name}", new_value
-      end
+        define_singleton_method "#{name}=" do |new_value|
+          instance_variable_set "@#{name}", new_value
+        end
 
-      if default != nil
-        instance_variable_set "@#{name}", default
+        if default != nil
+          instance_variable_set "@#{name}", default
+        end
       end
     end
-  end
   end
 
   using ModuleAttribute
